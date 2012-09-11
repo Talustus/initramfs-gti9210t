b2a44dae97082e5830875e66f67371a78403ad23
Talustus <talustus@dream-irc.de>
2012-09-11 06:11:59 +0200

    implement simple Extweaks interface

--- sbin/ext/post-init.sh
+++ sbin/ext/post-init.sh
@@ -9,17 +9,32 @@ mv /data/.dream/post-init.log /data/.dream/post-init.log.bak
 busybox date >/data/.dream/post-init.log
 exec >>/data/.dream/post-init.log 2>&1
 
-# Set Custom Kernel properties
+
+ccxmlsum=`md5sum /res/customconfig/customconfig.xml | awk '{print $1}'`
+if [ "a${ccxmlsum}" != "a`cat /data/.dream/.ccxmlsum`" ];
+then
+  rm -f /data/.dream/*.profile
+  echo ${ccxmlsum} > /data/.dream/.ccxmlsum
+fi
+[ ! -f /data/.dream/default.profile ] && cp /res/customconfig/default.profile /data/.dream
+[ ! -f /data/.dream/battery.profile ] && cp /res/customconfig/battery.profile /data/.dream
+[ ! -f /data/.dream/performance.profile ] && cp /res/customconfig/performance.profile /data/.dream
+
+. /res/customconfig/customconfig-helper
+read_defaults
+read_config
+
 /sbin/busybox sh /sbin/ext/properties.sh
 sleep 1
 
 # install Kernel related Apps etc
 /sbin/busybox sh /sbin/ext/install.sh
 
+# apply ExTweaks defaults
+/res/uci.sh apply
+
 ##### init scripts ########
 (
 /sbin/busybox sh /sbin/ext/run-init-scripts.sh
 )&
 
-busybox date >>/data/.dream/post-init.log
-echo "Post-Init finished"
